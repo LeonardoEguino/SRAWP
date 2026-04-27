7be76e98bd88ca8022f036042681e0eb52e1f853
Leonardo Eguino <leonardoeguinorv@gmail.com>
2026-04-27 09:29:07 -0700

    feat: implemented models for program and academic module, solved dev problems with tsconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { AcademicCoreModule } from './academic-core/academic-core.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AcademicCoreModule],
   controllers: [],
   providers: [],
 })
