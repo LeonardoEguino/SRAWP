89987edf0de8cd3c4c8cde0e1369726c15bd75f7
Leonardo Eguino <leonardoeguinorv@gmail.com>
2026-04-27 12:36:01 -0700

    feat: implemented whatsapp service

--- src/whatsapp/whatsapp.module.ts
+++ src/whatsapp/whatsapp.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { WhatsappService } from './whatsapp.service';
 
-@Module({})
+@Module({
+  providers: [WhatsappService]
+})
 export class WhatsappModule {}
