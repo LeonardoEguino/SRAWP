be60a81f6da286a49aa2c7c531c7832d82639075
Leonardo Eguino <leonardoeguinorv@gmail.com>
2026-04-27 12:42:15 -0700

    feat: implemented strategy at whatsapp module

--- src/whatsapp/whatsapp.module.ts
+++ src/whatsapp/whatsapp.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { WhatsappService } from './whatsapp.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WhatsappSession } from './entities/whatsapp-session.entity';
+import { WHATSAPP_PROVIDER } from './providers/whatsapp-provider.interface';
+import { BaileysProvider } from './providers/baileys/baileys.provider';
 
 @Module({
-  providers: [WhatsappService]
+  imports: [TypeOrmModule.forFeature([WhatsappSession])],
+  providers: [
+    WhatsappService,
+    {
+      provide: WHATSAPP_PROVIDER,
+      useClass: BaileysProvider, // <-- Cambiar para futuros providers (meta, twilio, etc...)
+    },
+  ],
+  exports: [WhatsappService],
 })
 export class WhatsappModule {}
